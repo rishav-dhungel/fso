98f0cdd3601b9ce90215846cf1aa06ef354fd111
rishav <rishavdhungel1@gmail.com>
2025-09-30 13:21:42 +0545

    feat(Exercises 1.3.-1.5)
    refer: https://fullstackopen.com/en/part1/java_script#exercises-1-3-1-5

--- part0/courseinfo/src/App.jsx
+++ part0/courseinfo/src/App.jsx
@@ -1,17 +1,26 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course}/>
-      <Content part1 = {part1} part2 ={part2} part3={part3} exercises1 = {exercises1} exercises2={exercises2} exercises3 = {exercises3}/>
-      <Total exercises1 = {exercises1} exercises2={exercises2} exercises3 = {exercises3}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
@@ -31,22 +40,26 @@ const Part = ({part, exercise}) =>{
   )
 }
 
-const Content = ({part1,part2, part3,exercises1, exercises2,exercises3}) =>{
+const Content = ({parts}) =>{
     return (
     <>
-      <Part part = {part1} exercise={exercises1}/>
-      <Part part = {part2} exercise={exercises2}/>
-      <Part part = {part3} exercise={exercises3}/>
+    { parts.map(part =>{
+      return <Part part={part.name} exercise={part.exercises}/>
+    })}
 
     </>
   )
 
 }
 
-const Total = ({exercises1, exercises2, exercises3}) =>{
+const Total = ({parts}) =>{
+    let total = 0
+    parts.forEach(part =>{
+      total += part.exercises
+    })
     return (
     <>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {total}</p>
     </>
   )
 
